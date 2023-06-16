60bb94149a53b241c626bf61857d524e3183842d
JonatasSilva01 <jhownsantossilva@gmail.com>
2023-06-16 15:00:07 -0300

    feat: atualizando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import Navbar from "./components/Navbar/Navbar";
-<<<<<<< HEAD
-import Footer from "./components/Footer/Footer";
-=======
 import { GlobalStyle } from "./styles/global-styles";
->>>>>>> 749b55ac27e255787726b62daff6f4ef80d3c301
 
 function App() {
   return (
